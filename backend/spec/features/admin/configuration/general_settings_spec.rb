66620e742e7b837ece6da3d4a2556cda586078b7
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-04-06 13:06:45 -0700

    Use breadcrumbs
    We add breadcrumbs to the page title to replace the "back_to_thing"
    button and change the arrow icon to a simple bracket to give a more
    common view of a hierarchy.

--- backend/spec/features/admin/configuration/general_settings_spec.rb
+++ backend/spec/features/admin/configuration/general_settings_spec.rb
@@ -11,12 +11,11 @@ describe "General Settings", type: :feature, js: true do
   before(:each) do
     visit spree.admin_path
     click_link "Settings"
-    click_link "General Settings"
   end
 
   context "visiting general settings" do
     it "should have the right content" do
-      expect(page).to have_content("General Settings")
+      expect(page).to have_content("Settings / General / Settings")
       expect(page).to have_field("store_name", with: "Test Store")
       expect(page).to have_field("store_url", with: "test.example.org")
       expect(page).to have_field("store_mail_from_address", with: "test@example.org")
